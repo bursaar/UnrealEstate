8989d7b777bdd92b4293f5100deebad2132f3509
Ben Keenan <ben@ben.ie>
2014-08-25 00:21:52 +0100

    Added Esc to Quit.

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour {
+
+
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		
+	}
+}
