10daa16f8e4db58e729a5ee04979b9fe58d922f6
Ben Keenan <ben@ben.ie>
2014-08-24 01:32:48 +0100

    Added notes for a class that would create readable strings from large numbers.

---
+++ Assets/Denominator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static class Denominator {
+
+	public static string NumbersToString(int number)
+	{
+	
+	// Create a string here
+	// Find out how big the number is.
+		// Greater than 1000000000 - billions.
+			// Divide by 1000000000, multiply by 100, add to the string after a decimal point.
+		// Greater than 1000000 - millions.
+			// Divide by 1000000, multiply by 100, add to the string after a decimal point.
+		// Greater than 1000 - thousands.
+			// Divide by 1000, multiply by 100, add to the string after a decimal point.
+			
+		// Give the right append
+		
+		// Return the amount (might have to return with the above magnitudes run in series.
+		
+		
+		return "NO_VAL";
+	}
+}
